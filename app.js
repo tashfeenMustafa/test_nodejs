aa0252497c382d5cd714067ad8e223e4c29fe1e3
Tashfeen Mustafa Choudhury <tashfeenc1234@gmail.com>
2022-07-25 23:50:29 +0600

    third commit, event loops, event emitter, async patterns

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-// npm - global command, comes with node
-// npm --version
+const EventEmitter = require('events');
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
+const customEmitter = new EventEmitter();
 
-// global dependency - use it in any project
-// npm install -g <packageName>
+customEmitter.on('response', () => {
+  console.log(`data received `)
+});
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties, etc.)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+customEmitter.on('response', () => {
+  console.log(`some other logic here `)
+});
 
-const _ = require('lodash');
-
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log('hello people');
+customEmitter.emit('response');
